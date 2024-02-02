5aacba6ae5054508082df0a7630e0ad19de2af82
tianlehou <86812844+tianlehou@users.noreply.github.com>
2024-02-02 00:07:38 -0500

    Add files via upload

---
+++ php/login_usuario_be.php
@@ -0,0 +1,26 @@
+<?php
+
+    session_start();
+
+    include 'conexion_be.php';
+
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+    $contrasena = hash('sha512', $contrasena); //Encriptar contraseña
+
+$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contrasena'");     //ok
+
+if(mysqli_num_rows($validar_login) > 0){
+    $_SESSION['usuario'] = $correo;
+    header("location: ../bienvenida.php");
+    exit;
+}else{
+    echo '
+        <script>
+            alert("Usuario no existe, por favor verifique los datos introducido");
+            window.location = "../index.php";
+        </script>
+    ';
+    exit;
+}
+?>
