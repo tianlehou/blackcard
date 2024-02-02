4464d9e0c5300ad2bee5220a7494a6d14df7ae6e
Tian Le Hou <tianlehou1983@gmail.com>
2024-02-02 00:38:14 -0500

    Create bienvenida.php

---
+++ bienvenida.php
@@ -0,0 +1,60 @@
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        echo '
+            <script>
+                alert("Por favor debes iniciar sesión");
+                window.location = "index.php";
+            </script>
+        ';
+        session_destroy();
+        die();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida</title>
+</head>
+<body>
+    <h1>Bienvenido a esta página</h1>
+    <a href="php/cerrar_sesion.php">Cerrar sesión</a>
+</body>
+</html>
+
+=======
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        echo '
+            <script>
+                alert("Por favor debes iniciar sesión");
+                window.location = "index.php";
+            </script>
+        ';
+        session_destroy();
+        die();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida</title>
+</head>
+<body>
+    <h1>Bienvenido a esta página</h1>
+    <a href="php/cerrar_sesion.php">Cerrar sesión</a>
+</body>
+</html>
+
+>>>>>>> 5aacba6ae5054508082df0a7630e0ad19de2af82
